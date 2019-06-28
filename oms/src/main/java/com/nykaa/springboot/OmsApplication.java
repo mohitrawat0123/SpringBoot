e95b45ad2f59f01e7cfaa05074c14da31713f3a2
mohitrawat0123 <mohitrawat0123@gmail.com>
2019-06-28 16:26:34 +0530

    b1

--- oms/src/main/java/com/nykaa/springboot/OmsApplication.java
+++ oms/src/main/java/com/nykaa/springboot/OmsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OmsApplication {
 
 	public static void main(String[] args) {
-		//in branch1
+		//branch1
 		SpringApplication.run(OmsApplication.class, args);
 	}
 
