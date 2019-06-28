2787533d6a36b89defc65adbe60c03e1f61f2995
mohitrawat0123 <mohitrawat0123@gmail.com>
2019-06-28 17:16:07 +0530

    hey

--- oms/src/main/java/com/nykaa/springboot/OmsApplication.java
+++ oms/src/main/java/com/nykaa/springboot/OmsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OmsApplication {
 
 	public static void main(String[] args) {
+		//comment
 		SpringApplication.run(OmsApplication.class, args);
 	}
 
