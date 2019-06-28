690b1a9ca6532058f832bccf7b6b8b458c6c936c
mohitrawat0123 <mohitrawat0123@gmail.com>
2019-06-28 13:59:03 +0530

    removed comment

--- oms/src/main/java/com/nykaa/springboot/OmsApplication.java
+++ oms/src/main/java/com/nykaa/springboot/OmsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OmsApplication {
 
 	public static void main(String[] args) {
-		//comment 
 		SpringApplication.run(OmsApplication.class, args);
 	}
 
